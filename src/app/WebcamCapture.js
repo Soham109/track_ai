8fb7d97f63fd55564406c19c04d3da974b548fe3
Soham Aggarwal <aggarwalsoham3@gmail.com>
2024-08-03 20:04:27 +0530

    some more changes...

--- src/app/WebcamCapture.js
+++ src/app/WebcamCapture.js
@@ -8,6 +8,8 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
 import './WebcamCapture.css';
+import { Snackbar, Alert } from '@mui/material';
+import Backdrop from '@mui/material/Backdrop';
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
@@ -30,23 +32,27 @@ const WebcamCapture = () => {
   const uploadImage = async () => {
     if (image) {
       setMessage("Uploading photo...");
-
       const response = await fetch(image);
       const blob = await response.blob();
 
-      const apiResponse = await fetch(`https://image-intellect-ai.cognitiveservices.azure.com/vision/v3.2/analyze?visualFeatures=Categories,Description,Color`, {
-        method: 'POST',
-        headers: {
-          'Ocp-Apim-Subscription-Key': '79892cfe82f0486a9e571b26140613a1',
-          'Content-Type': 'application/octet-stream',
-        },
-        body: blob,
-      });
+      try {
+        const apiResponse = await fetch(`https://image-intellect-ai.cognitiveservices.azure.com/vision/v3.2/analyze?visualFeatures=Categories,Description,Color`, {
+          method: 'POST',
+          headers: {
+            'Ocp-Apim-Subscription-Key': '79892cfe82f0486a9e571b26140613a1',
+            'Content-Type': 'application/octet-stream',
+          },
+          body: blob,
+        });
 
-      const result = await apiResponse.json();
-      const formattedText = formatResponse(result);
-      setMessage(formattedText);
-      setItemName(formattedText);
+        const result = await apiResponse.json();
+        const formattedText = formatResponse(result);
+        setMessage(formattedText);
+        setItemName(formattedText);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        setMessage("Failed to upload image");
+      }
     }
   };
 
@@ -59,24 +65,28 @@ const WebcamCapture = () => {
 
   const addItem = async () => {
     if (itemName && quantity && user) {
-      await addDoc(collection(db, 'pantry'), {
-        name: itemName,
-        quantity: parseInt(quantity),
-        createdAt: new Date(),
-        userId: user.uid,
-      });
-      setMessage("Item added to pantry");
-      fetchPantryItems();
+      try {
+        await addDoc(collection(db, 'pantry'), {
+          name: itemName,
+          quantity: parseInt(quantity),
+          createdAt: new Date(),
+          userId: user.uid,
+        });
+        setMessage("Item added to pantry");
+        fetchPantryItems();
+      } catch (error) {
+        console.error("Error adding item:", error);
+        setMessage("Failed to add item");
+      }
     }
   };
+
   const fetchPantryItems = async () => {
     if (user) {
       setLoading(true);
       try {
-        console.log("Fetching pantry items for user:", user.uid);
         const q = query(collection(db, 'pantry'), where('userId', '==', user.uid));
         const snapshot = await getDocs(q);
-        console.log("Snapshot data:", snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
         const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         setPantryItems(items);
         setMessage("Pantry items fetched successfully");
@@ -88,24 +98,33 @@ const WebcamCapture = () => {
       }
     }
   };
-  
 
   const updateItem = async (id, updatedQuantity) => {
     if (updatedQuantity < 0) {
       setMessage("Quantity cannot be less than zero");
       return;
     }
-    await updateDoc(doc(db, 'pantry', id), { quantity: updatedQuantity });
-    setMessage("Item updated");
-    fetchPantryItems();
+    try {
+      await updateDoc(doc(db, 'pantry', id), { quantity: updatedQuantity });
+      setMessage("Item updated");
+      fetchPantryItems();
+    } catch (error) {
+      console.error("Error updating item:", error);
+      setMessage("Failed to update item");
+    }
   };
 
   const removeItem = async (id) => {
-    await deleteDoc(doc(db, 'pantry', id));
-    setMessage("Item removed");
-    fetchPantryItems();
+    console.log('Removing item with id:', id);
+    try {
+      await deleteDoc(doc(db, 'pantry', id));
+      setMessage("Item removed");
+      fetchPantryItems();
+    } catch (error) {
+      console.error("Error removing item:", error);
+      setMessage("Failed to remove item");
+    }
   };
-
   const handleSignOut = async () => {
     try {
       await signOut(auth);
@@ -119,44 +138,54 @@ const WebcamCapture = () => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       if (currentUser) {
-        fetchPantryItems();
+        fetchPantryItems(); 
       } else {
         setPantryItems([]);
       }
     });
-  
-    return () => unsubscribe(); 
+
+    return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      fetchPantryItems();
+    }
+  }, [user]);
+
 
   const filteredPantryItems = pantryItems.filter(item =>
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
   return (
-    <Container>
+    <Container maxWidth="lg" sx={{ padding: '20px', backgroundColor: '#f7f7f7' }}>
       {user ? (
         <>
-          <AppBar position="static">
+          <AppBar position="static" sx={{ backgroundColor: '#333', color: '#fff' }}>
             <Toolbar>
-              <Typography variant="h6" style={{ flexGrow: 1 }}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 Pantry
               </Typography>
               {user.photoURL && (
                 <img src={user.photoURL} alt="Profile" className="profile-pic" style={{ borderRadius: '50%', width: '40px', marginRight: '10px' }} />
               )}
-              <Typography variant="body1" style={{ marginRight: '10px' }}>
+              <Typography variant="body1" component="div" sx={{ marginRight: '10px' }}>
                 {user.displayName || user.email}
               </Typography>
-              <Button onClick={handleSignOut} variant="contained" color="secondary">
+              <Button onClick={handleSignOut} variant="contained" color="secondary" sx={{ backgroundColor: '#333', color: '#fff' }}>
                 Sign Out
               </Button>
             </Toolbar>
           </AppBar>
-          <Typography variant="h4" gutterBottom>TrackAI</Typography>
+          <Typography variant="h4" component="div" sx={{ marginBottom: '20px', fontWeight: 'bold' }}>
+            TrackAI
+          </Typography>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6}>
-              <Paper elevation={3} style={{ padding: '16px' }}>
-                <Typography variant="h6" gutterBottom>Capture Photo</Typography>
+              <Paper elevation={3} sx={{ padding: '20px', backgroundColor: '#fff' }}>
+                <Typography variant="h6" component="div" sx={{ marginBottom: '10px' }}>
+                  Capture Photo
+                </Typography>
                 <div className="webcam-container">
                   <div className="webcam-wrapper">
                     <Webcam
@@ -165,14 +194,14 @@ const WebcamCapture = () => {
                       screenshotFormat="image/jpeg"
                       className="webcam"
                     />
-                    <Button onClick={capture} variant="contained" color="primary" fullWidth style={{ marginBottom: '20px' }}>
+                    <Button onClick={capture} variant="contained" color="primary" fullWidth sx={{ marginBottom: '10px' }}>
                       Capture Photo
                     </Button>
                   </div>
                   {image && (
                     <div className="webcam-wrapper" style={{ marginTop: '0px' }}>
                       <img src={image} alt="Captured" className="captured-image" />
-                      <Button onClick={uploadImage} variant="contained" color="secondary" fullWidth style={{ marginBottom: '20px' }}>
+                      <Button onClick={uploadImage} variant="contained" color="secondary" fullWidth sx={{ marginBottom: '10px' }}>
                         Upload and Analyze
                       </Button>
                     </div>
@@ -181,15 +210,17 @@ const WebcamCapture = () => {
               </Paper>
             </Grid>
             <Grid item xs={12} md={6}>
-              <Paper elevation={3} style={{ padding: '16px' }}>
-                <Typography variant="h6" gutterBottom>Add or Update Pantry Item</Typography>
+              <Paper elevation={3} sx={{ padding: '20px', backgroundColor: '#fff' }}>
+                <Typography variant="h6" component="div" sx={{ marginBottom: '10px' }}>
+                  Add or Update Pantry Item
+                </Typography>
                 <TextField
                   label="Item Name"
                   variant="outlined"
                   fullWidth
                   value={itemName}
                   onChange={(e) => setItemName(e.target.value)}
-                  style={{ marginBottom: '16px' }}
+                  sx={{ marginBottom: '10px' }}
                 />
                 <TextField
                   label="Quantity"
@@ -198,55 +229,64 @@ const WebcamCapture = () => {
                   fullWidth
                   value={quantity}
                   onChange={(e) => setQuantity(e.target.value)}
-                  style={{ marginBottom: '16px' }}
+                  sx={{ marginBottom: '10px' }}
                 />
                 <Button onClick={addItem} variant="contained" color="primary" fullWidth>
                   Add Item
                 </Button>
               </Paper>
             </Grid>
-                <Grid item xs={12}>
-      <Paper elevation={3} style={{ padding: '16px' }}>
-        <Typography variant="h6" gutterBottom>Search Pantry Items</Typography>
-        <TextField
-          label="Search Items"
-          variant="outlined"
-          fullWidth
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          style={{ marginBottom: '16px' }}
-        />
-        <Typography variant="h6" gutterBottom>Pantry Items</Typography>
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          filteredPantryItems.length > 0 ? (
-            filteredPantryItems.map(item => (
-              <Card key={item.id} style={{ marginBottom: '16px' }}>
-                <CardContent>
-                  <Typography variant="h6">{item.name}</Typography>
-                  <Typography>Quantity: {item.quantity}</Typography>
-                  <Button onClick={() => updateItem(item.id, item.quantity + 1)} variant="contained" color="primary" style={{ marginRight: '8px' }}>
-                    +
-                  </Button>
-                  <Button onClick={() => updateItem(item.id, item.quantity - 1)} variant="contained" color="primary" style={{ marginRight: '8px' }}>
-                    -
-                  </Button>
-                  <Button onClick={() => removeItem(item.id)} variant="contained" color="secondary">
-                    Remove Item
-                  </Button>
-                </CardContent>
-              </Card>
-            ))
-          ) : (
-            <Typography variant="body1">No items found.</Typography>
-          )
-        )}
-      </Paper>
-    </Grid>
-
+            <Grid item xs={12}>
+              <Paper elevation={3} sx={{ padding: '20px', backgroundColor: '#fff' }}>
+                <Typography variant="h6" component="div" sx={{ marginBottom: '10px' }}>
+                  Search Pantry Items
+                </Typography>
+                <TextField
+                  label="Search Items"
+                  variant="outlined"
+                  fullWidth
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  sx={{ marginBottom: '10px' }}
+                />
+                <Typography variant="h6" component="div" sx={{ marginBottom: '10px' }}>
+                  Pantry Items
+                </Typography>
+                {loading ? (
+                  <CircularProgress />
+                ) : (
+                  filteredPantryItems.length > 0 ? (
+                    filteredPantryItems.map(item => (
+                      <Card key={item.id} sx={{ marginBottom: '10px', backgroundColor: '#fff' }}>
+                        <CardContent>
+                          <Typography variant="h6" component="div" sx={{ marginBottom: '5px' }}>
+                            {item.name}
+                          </Typography>
+                          <Typography variant="body1" component="div" sx={{ marginBottom: '5px' }}>
+                            Quantity: {item.quantity}
+                          </Typography>
+                          <Button onClick={() => updateItem(item.id, item.quantity + 1)} variant="contained" color="primary" sx={{ marginRight: '5px' }}>
+                            +
+                          </Button>
+                          <Button onClick={() => updateItem(item.id, item.quantity - 1)} variant="contained" color="primary" sx={{ marginRight: '5px' }}>
+                            -
+                          </Button>
+                          <Button onClick={() => removeItem(item.id)} variant="contained" color="secondary">
+                          Remove Item
+                          </Button>
+                        </CardContent>
+                      </Card>
+                    ))
+                  ) : (
+                    <Typography variant="body1" component="div">
+                      No items found.
+                    </Typography>
+                  )
+                )}
+              </Paper>
+            </Grid>
           </Grid>
-          <Typography variant="body1" color="textSecondary" style={{ marginTop: '16px' }}>
+          <Typography variant="body1" component="div" sx={{ marginTop: '20px' }}>
             {message}
           </Typography>
         </>
@@ -255,20 +295,33 @@ const WebcamCapture = () => {
           {showSignUp ? (
             <>
               <SignUpForm onSignUp={() => setUser(auth.currentUser)} />
-              <Button onClick={() => setShowSignUp(false)} variant="contained" color="primary" style={{ marginTop: '16px' }}>
+              <Button onClick={() => setShowSignUp(false)} variant="contained" color="primary" sx={{ marginTop: '10px' }}>
                 Already have an account? Sign In
               </Button>
             </>
           ) : (
             <>
               <SignInForm onSignIn={() => setUser(auth.currentUser)} />
-              <Button onClick={() => setShowSignUp(true)} variant="contained" color="primary" style={{ marginTop: '16px' }}>
+              <Button
+                onClick={() => setShowSignUp(true)}
+                variant="contained"
+                color="primary"
+                sx={{ marginTop: '10px' }}
+              >
                 Need an account? Sign Up
               </Button>
             </>
           )}
         </div>
       )}
+      <Snackbar open={!!message} autoHideDuration={6000} onClose={() => setMessage('')}>
+        <Alert onClose={() => setMessage('')} severity="success">
+          {message}
+        </Alert>
+      </Snackbar>
+      <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </Container>
   );
 };
